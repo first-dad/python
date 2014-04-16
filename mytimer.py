fcaebe4031366286fe7be9e2bf0b504964ddacf0
first_dad <first_dad@mail.ru>
2014-04-16 14:45:50 +0400

    new

--- mytimer.py
+++ mytimer.py
@@ -3,15 +3,32 @@
 
 # Файл mytimer.py
 
-import time
-reps = 1000
-repslist = range(reps)
+import time, sys
+if sys.platform[:3] == 'win':
+    timefunc = time.clock
+else:
+    timefunc = time.time
+
+def trace(*args): pass
 
 def timer(func, *pargs, **kargs):
-    start = time.clock()
+    _reps = kargs.pop('_reps', 1000)
+
+    trace(func, pargs, kargs, _reps)
+    repslist = range (_reps)
+
+    start = timefunc()
     for i in repslist:
         ret = func(*pargs,**kargs)
-    elapsed = time.clock() - start
+    elapsed = timefunc() - start
     return(elapsed, ret)
 
+def best(func, *pargs, **kargs):
+    _reps = kargs.pop('_reps', 50)
+    best = 2 ** 32
+    for i in range(_reps):
+        time, ret = timer(func, *pargs, _reps = 1, **kargs)
+        if time < best: best = time
+    return best, ret
 if __name__ == '__main__':
+    pass
--- timeseqs.py
+++ timeseqs.py
@@ -9,28 +9,28 @@ repslist = range(reps)
 def forLoop():
     res = []
     for x in repslist:
-        res.append(abs(x))
+        res.append(x + 10)
     return res
 
 def listComp():
-    return [abs(x) for x in repslist]
+    return [x + 10 for x in repslist]
 
 def mapCall():
-    return list(map(abs, repslist))
+    return list(map(lambda x: x + 10, repslist))
 
 def genExpr():
-    return list(abs(x) for x in repslist)
+    return list(x + 10 for x in repslist)
 
 def genFunc():
     def gen():
         for x in repslist:
-            yield abs(x)
-        return list(gen())
-
-print (sys.version)
-for test in (forLoop, listComp, mapCall, genExpr, genFunc):
-    elapsed, result = mytimer.time(test)
-    print ('-' * 33)
-    print('%-9s: %.5f => [%s...%s]' %(test.__name__, elapsed, result[0], result[-1]))
-
+            yield x + 10
+    return list(gen())
 if __name__ == '__main__':
+    print (sys.version)
+    for tester in (mytimer.timer, mytimer.best):
+        print('<%s>' % tester.__name__)
+        for test in (forLoop, listComp, mapCall, genExpr, genFunc):
+            elapsed, ret = tester(test)
+            print ('-' * 35)
+            print('%-9s: %.5f => [%s...%s]' %(test.__name__, elapsed, ret[0], ret[-1]))
